cf1eccd69c8588cce1649d80f92e596a3d09a67f
Marcopole11 <nicomarcopernic@gmail.com>
2025-02-01 20:44:03 +0100

    colores random

--- godot-project/scripts/playerColor.gd
+++ godot-project/scripts/playerColor.gd
@@ -2,14 +2,23 @@ extends Node3D
 
 @export var playerLegs:MeshInstance3D;
 @export var playerTorso:MeshInstance3D;
+@export var playerGoogles:MeshInstance3D;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	playerLegs.mesh = playerLegs.mesh.duplicate();
+	playerTorso.mesh = playerTorso.mesh.duplicate();
+	playerGoogles.mesh = playerGoogles.mesh.duplicate();
 	var newCoth:Material = playerLegs.mesh.surface_get_material(0).duplicate();
-	newCoth.albedo_color = Color(randf() , randf(), randf(), 1);
+	var newBright:Material = playerLegs.mesh.surface_get_material(1).duplicate();
+	var charColor:Color = Color(randf()/4 , randf()/4, randf()/4, 1);
+	newCoth.albedo_color = Color(charColor.r+0.3,charColor.g+0.3,charColor.b+0.3,1);
+	newBright.albedo_color = Color(charColor.r*4,charColor.g*4,charColor.b*4,1);
 	playerLegs.mesh.surface_set_material(0,newCoth);
+	playerTorso.mesh.surface_set_material(1,newBright);
+	playerTorso.mesh.surface_set_material(3,newCoth);
+	playerGoogles.mesh.surface_set_material(1,newBright);
 	
 	pass # Replace with function body.
 
