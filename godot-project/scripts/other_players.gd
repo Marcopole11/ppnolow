c8c48b140ebb04e97b15643a1c6b752be59f1ad7
Marcopole11 <nicomarcopernic@gmail.com>
2025-02-02 18:07:08 +0100

    asigned other players

---
+++ godot-project/scripts/other_players.gd
@@ -0,0 +1,20 @@
+# entity_movement.gd script
+
+# extend the functionality of your root node (here Node3D)
+extends Node3D
+
+# when the scene is loaded
+func _ready():
+	# connect to the state_changed signal from pp_entity_node
+	var pp_entity_node= get_node_or_null("PPEntityNode")
+	if pp_entity_node:
+		pp_entity_node.state_changed.connect(_on_state_changed)
+	else:
+		print("PPEntityNode not found")
+
+func _on_state_changed(state):
+	# set the entity's position, using the server's values
+	# NOTE: Planetary Processing uses 'y' for depth in 3D games, and 'z' for height. The depth axis is also inverted.
+	# To convert, set Godot's 'y' to negative, then swap 'y' and 'z'.
+	print(state);
+	global_transform.origin = Vector3(state.x, state.z, -state.y) 
