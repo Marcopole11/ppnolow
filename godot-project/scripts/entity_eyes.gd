6e0b7bc93e256c48f4dea6e956098a265a3d13e0
Marcopole11 <nicomarcopernic@gmail.com>
2025-02-07 22:12:21 +0100

    ojos se mueven en todas las direcciones

--- godot-project/scripts/entity_eyes.gd
+++ godot-project/scripts/entity_eyes.gd
@@ -21,7 +21,7 @@ func _physics_process(delta):
 		if playergroup.size() > 0:
 			player = playergroup[0]
 	else:
-		var tall = 3.5;
+		var tall = 4;
 		for eye in range(eyes.size()):
 			eyes[eye].look_at(player.position+Vector3(0,tall,0))
 			var eyemesh:Node3D = eyes[eye].get_node("EyeMesh")
@@ -35,20 +35,36 @@ func _physics_process(delta):
 func babyStep(rad:float,deg:int):
 	var org = rad_to_deg(rad)
 	var retorno = deg
+	var speed = (abs((org-deg)/4)+1)/5
 	if org == deg:
 		return rad
 	if org < deg:
-		if org+2 > deg: return deg_to_rad(deg)
-		else: return deg_to_rad(org+2)
+		if org+speed > deg: return deg_to_rad(deg)
+		else: return deg_to_rad(org+speed)
 	else:
-		if org-2 < deg: return deg_to_rad(deg)
-		else: return deg_to_rad(org-2)
+		if org-speed < deg: return deg_to_rad(deg)
+		else: return deg_to_rad(org-speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	frame=frame+1
-	if frame > 5000:
+	if frame > 30:
 		frame = 0
+		var spotted = 0;
 		for eye in range(eyes.size()):
-			eyesJobY[eye] = 0;
-			eyesJobZ[eye] = 0;
+			eyesJobY[eye] = randomSpot(eyesJobY[eye])
+			eyesJobZ[eye] = randomSpot(eyesJobZ[eye])
+			if eyesJobY[eye] == 90 and eyesJobZ[eye] == 90:
+				spotted = spotted + 1
+		if spotted > 3:
+			eyesJobY[0] = 90
+			eyesJobZ[0] = 90
+
+func randomSpot(old:int):
+	var rot = randi_range(-1400,400)
+	if rot<0:
+		return old
+	if rot>180:
+		return 90
+	return rot
+	
