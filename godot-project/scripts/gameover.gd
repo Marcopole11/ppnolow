6f5e2067314af3312cd13e2b66d84f7894c0fa9f
Kbloody <elchollodeambos@gmail.com>
2025-02-09 17:09:19 +0100

    winmenu arreglao

--- godot-project/scripts/gameover.gd
+++ godot-project/scripts/gameover.gd
@@ -6,14 +6,7 @@ var randomizer:int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	randomizer = randi_range(0,50)
-	label.show()
-	label_2.hide()
-	if randomizer == 1:
-		label_2.show()
-		label.hide()
-		mantis_walk.play("idle")
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
